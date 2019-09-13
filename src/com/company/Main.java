ecab772326d4464e535ce42401d481ebf409dd68
brandonbouley <brandonbouley@billericak12.com>
2019-09-13 10:58:21 -0400

    the

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.management.QueryEval;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String a= "Funy Monky....";
+        String b= ":P";
+
+        System.out.println(a);
+        System.out.println(b);
+        
+
+
+    }
+}
